317daadf24fb4fc7f9185e27eee26cbc94d27fc6
jeel-a-simformsolutions <jeel.a@simformsolutions.com>
2021-01-27 10:26:23 +0530

    add file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [num, setnum] = useState(0);
+
+  const incNum = () => {
+    setnum(num + 1);
+  };
+
+  const decNum = () => {
+    if (num > 0) {
+      setnum(num - 1);
+    } else {
+      alert('sorry, zero limit reached')
+      setnum(0);
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <h1> {num} </h1>
+          <div className="btn_div">
+            <button onClick={incNum}> Increment </button>
+            <button onClick={decNum}> decrement </button>
+          </div>
+
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
